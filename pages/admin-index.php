ec3d20126288b89c5dade186a7037ad906fab0bd
gerogealexandruiancu <iancugeorgealexandru@gmail.com>
2020-01-05 15:40:15 +0200

    admin login page

--- pages/admin-index.php
+++ pages/admin-index.php
@@ -11,15 +11,14 @@
 		$count = mysqli_num_rows($result);
 		$row = $result->fetch_assoc();
 		if($count == 1){
-			$_SESSION['username'] = $username;
+			$_SESSION['username'] = $row['name'];
 			$_SESSION['connected'] = true;
 			$_SESSION['role'] = $row['role'];
-		
 		}else{
 			$fmsg = "Invalid Username/Password";
 		}
 	}
-	if(isset($_SESSION['connected']) == true){
+	if(isset($_SESSION['connected']) == true && $_SESSION['role'] == 'administrator'){
 		header("Location: dashboard.php");
 	}
 ?>
@@ -38,16 +37,20 @@
 					<!-- start: page -->
 					<div class="u-margin-top-double">
 						<h1>Admin Weather Locations</h1>
-						<form>
+						
+						<form method="post">
 							<div class="form-group">
 								<label for="exampleInputEmail1">Email address</label>
-								<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+								<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="adminEmail">
 								<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 							</div>
 							<div class="form-group">
 								<label for="exampleInputPassword1">Password</label>
-								<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+								<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="adminPassword">
 							</div>
+							<?php if($fmsg != "") {?>
+							<p class="o-title o-title__text-danger  u-font-size__l"><?php echo $fmsg; ?></p>
+							<?php } ?>
 							<button type="submit" class="btn btn-primary">Submit</button>
 						</form>
 					</div>
